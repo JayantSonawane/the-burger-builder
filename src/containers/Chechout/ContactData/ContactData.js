619c61087dbab6d2d1eb64a807217e0c33e9fc78
Jayant Sonawane <sonawanejayant1@gmail.com>
2020-11-05 07:49:26 +0530

    Burger Builder Before Redux Adv

--- src/containers/Chechout/ContactData/ContactData.js
+++ src/containers/Chechout/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Button from '../../../components/UI/Button/Button';
 import Spinner from '../../../components/UI/Spinner/Spinner';
@@ -103,7 +104,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
         }
         const order = {
-            ingredients: this.props.ingredients,
+            ingredients: this.props.ings,
             price: this.props.price,
             orderData: formData
         }
@@ -164,7 +165,6 @@ class ContactData extends Component {
         for (let inputIdentifier in updatedOrderForm) {
             formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
         }
-
         this.setState({ orderForm: updatedOrderForm, formIsValid: formIsValid });
     }
 
@@ -204,4 +204,11 @@ class ContactData extends Component {
     }
 }
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings: state.ingredients,
+        price: state.total
+    }
+};
+
+export default connect(mapStateToProps)(ContactData);
